1365618655fec64b5b1dc8300a28c2ff4c28b4a0
Mark Keogh <mkeogh@mediaocean.com>
2023-05-29 12:36:04 +1000

    Initial Commit

---
+++ src/ProcessTimer.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+namespace Pbp.Timers
+{
+    public sealed class ProcessTimer : IDisposable
+    {
+        private Action<TimeSpan>? callback;
+        private readonly Stopwatch stopwatch = new();
+
+        public ProcessTimer() => stopwatch.Start();
+
+        public static ProcessTimer RunOnComplete(Action<TimeSpan> callback)
+        {
+            return new ProcessTimer { callback = callback };
+        }
+
+        public void Dispose()
+        {
+            stopwatch.Stop();
+            callback?.Invoke(stopwatch.Elapsed);
+        }
+    }
+}
